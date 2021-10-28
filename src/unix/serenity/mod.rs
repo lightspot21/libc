209c7dbc4a60fad3259ea95ee4f3d58316234ada
Grigoris Pavlakis <grigpavl@ece.auth.gr>
2021-10-28 17:12:11 +0300

    Add lconv struct definition

--- src/unix/serenity/mod.rs
+++ src/unix/serenity/mod.rs
@@ -89,10 +89,41 @@ s! {
 pub const FD_SETSIZE: usize = 1024;
 s! {
     pub struct fd_set {
-        fds_bits: [::c_uchar, FD_SETSIZE / 8]
+        pub fds_bits: [::c_uchar, FD_SETSIZE / 8]
     }
 }
 // end fd_set.h
+
+// start locale.h
+s! {
+    pub struct lconv {
+        pub decimal_point: *mut ::c_char,
+        pub thousands_sep: *mut ::c_char,
+        pub grouping: *mut ::c_char,
+        pub int_curr_symbol: *mut ::c_char,
+        pub currency_symbol: *mut ::c_char,
+        pub mon_decimal_point: *mut ::c_char,
+        pub mon_thousands_sep: *mut ::c_char,
+        pub mon_grouping: *mut ::c_char,
+        pub positive_sign: *mut ::c_char,
+        pub negative_sign: *mut ::c_char,
+        pub int_frac_digits: ::c_char
+        pub frac_digits: ::c_char
+        pub p_cs_precedes: ::c_char
+        pub p_sep_by_space: ::c_char
+        pub n_cs_precedes: ::c_char
+        pub n_sep_by_space: ::c_char
+        pub p_sign_posn: ::c_char
+        pub n_sign_posn: ::c_char
+        pub int_p_cs_precedes: ::c_char
+        pub int_p_sep_by_space: ::c_char
+        pub int_n_cs_precedes: ::c_char
+        pub int_n_sep_by_space: ::c_char
+        pub int_p_sign_posn: ::c_char
+        pub int_n_sign_posn: ::c_char
+    }
+}
+// end locale.h
 
 // start LibDl/dlfcn.h
 s! {
