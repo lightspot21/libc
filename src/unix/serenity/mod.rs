b20376feb586edcd1677c8da15677d2f1c6f84f5
Grigoris Pavlakis <grigpavl@ece.auth.gr>
2021-10-28 16:45:44 +0300

    Add more types from sys/types.h, completely defined
    Added types:
    clock_t
    dev_t
    mode_t
    off_t
    pthread_attr_t
    pthread_cond_t
    pthread_condattr_t
    pthread_key_t
    pthread_mutexattr_t
    pthread_mutex_t
    pthread_rwlock_t
    pthread_rwlockattr_t
    pthread_t
    suseconds_t
    time_t

--- src/unix/serenity/mod.rs
+++ src/unix/serenity/mod.rs
@@ -1,3 +1,10 @@
+// Convention: If bitness is specified in the original C type,
+// then the corresponding Rust type is specified directly.
+// (eg. if original is uint64_t, the u64 type is used). If the
+// original C type does not specify bitness, then the corresponding
+// C mask is used.
+// (eg. if original is unsigned long, the c_ulong type will be used here).
+
 // C types not defined by unix family and inferred by limits.h
 pub type c_char = i8;
 pub type c_long = i32;
@@ -6,6 +13,37 @@ pub type c_ulong = u32;
 
 // start sys/types.h
 pub type socklen_t = u32; //Kernel/API/POSIX/sys/types.h:56
+pub type clock_t = u32;
+pub type dev_t = u32;
+pub type mode_t = u16; 
+pub type off_t = i64;
+pub type suseconds_t = i32;
+pub type time_t = i64;
+
+pub type pthread_attr_t = *mut ::c_void;
+pub type pthread_key_t = ::c_int;
+pub type pthread_rwlockattr_t = *mut ::c_void;
+pub type pthread_rwlock_t = u64;
+pub type pthread_t = ::c_int;
+s! {
+    pub struct pthread_cond_t {
+        mutex: *mut ::pthread_mutex_t,
+        value: u32,
+        clockid: ::c_int //clockid_t
+    }
+    pub struct pthread_condattr_t {
+        clockid: ::c_int //clockid_t
+    }
+    pub struct pthread_mutexattr_t {
+        type: ::c_int
+    }
+    pub struct pthread_mutex_t {
+        lock: u32,
+        owner: ::pthread_t,
+        level: ::c_int,
+        type: ::c_int
+    }
+}
 // end sys/types.h
 
 // start sys/socket.h
